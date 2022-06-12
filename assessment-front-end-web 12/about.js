e24836656f980607ae56280583416cd32b9c426a
Maginele Larins <100315803ml@gmail.com>
2022-06-12 17:22:27 -0600

    initialcommit

--- assessment-front-end-web 12/about.js
+++ assessment-front-end-web 12/about.js
@@ -8,13 +8,15 @@ function handleSubmit(evt) {
 	alert ('The form has been submitted');
 }
 
-
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
-// image.addEventListener("mouseover", function() {}
 
-// // let picture = document.getElementById("picture");
-// // picture.addEventListener("mouseover", function(event){
-// // 	event.target.picture
-// // }
+const j= () => {
+	var cat = document.querySelector('cat');
+	alert ("Well done!")
+}
+
+cat.addEventListener("mouseover", j);
+		
+	
--- assessment-front-end-web 12/profile.js
+++ assessment-front-end-web 12/profile.js
@@ -1,14 +1,9 @@
 
-color.addEventListener("click",function(){ alert("My Favorite Color"); });
-place.addEventListener("click",function(){ alert("My Favorite Place"); });
-ritual.addEventListener("click",function(){ alert("My Favorite Ritual"); });
+color.addEventListener("click",function(){ alert("Blue"); });
+place.addEventListener("click",function(){ alert("Santiago"); });
+ritual.addEventListener("click",function(){ alert("Sunday Games"); });
 
 
 
 
-var color=document.querySelector('#color');
-color.classList.add('class');
-var place=document.querySelector('#color');
-color.classList.add('class');
-var ritual=document.querySelector('#color');
-color.classList.add('class');
+
